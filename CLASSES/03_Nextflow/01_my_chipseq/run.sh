66919276f2fab9b747d4016b8b22ffe81c549b3d
Jessica Song <jeso3380@fiji-viz.colorado.edu>
2023-03-18 21:01:21 -0600

    done!

---
+++ CLASSES/03_Nextflow/01_my_chipseq/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH --job-name=PolII_test
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=jeso3380@colorado.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=6gb
+#SBATCH --time=20:00:00
+#SBATCH --output=nextflow.out
+#SBATCH --error=nextflow.err
+pwd; hostname; date
+echo "Lets go"
+module load singularity/3.1.1
+nextflow run nf-core/chipseq -r 1.2.1 \
+-profile singularity \
+--single_end \
+--input design.csv \
+--fasta /scratch/Shares/rinnclass/CLASS_2023/data/data/genomes/GRCh38.p13.genome.fa \
+--gtf /scratch/Shares/rinnclass/CLASS_2023/data/data/genomes/gencode.v32.annotation.gtf \
+--macs_gsize 3.2e9 \
+--blacklist /scratch/Shares/rinnclass/CLASS_2023/data/data/genomes/hg38-blacklist.v2.bed \
+--email jeso3380@colorado.edu \
+-resume \
+-c nextflow.config
+date
